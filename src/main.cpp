862c0dd7249727aaece7289ee966704195fb5fbf
Benjamin Wright <y0rke@protonmail.com>
2021-11-26 14:18:47 +0800

    List out modules

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <cstdio>
 #include <ncurses.h>
-#include <cstdlib>
+#include <clocale>
+#include <swmgr.h>
+#include <markupfiltmgr.h>
+
+using namespace sword;
 
 int main(int argc, char* argv[]){
-	printf("Hello World!\n");
+	//ncurses init
+	setlocale(LC_ALL, "");
+	initscr();
+	start_color();
+	cbreak();
+	noecho();
+	curs_set(0);
+	keypad(stdscr, TRUE);
+	refresh();
+
+	SWMgr library(new MarkupFilterMgr(FMT_PLAIN));
+	ModMap::iterator it;
+	for (it = library.Modules.begin(); it != library.Modules.end(); it++) {
+		printw("[%s]\t - %s\n", (*it).second->getName(), (*it).second->getDescription());
+	}
+
+	getch();
 }
